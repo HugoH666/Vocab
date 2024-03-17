1f74cac25966849f63720e6362b3988c59ea38be
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-17 23:19:35 +0100

    #4 feat Ajout du contenu de la liste Clothing

---
+++ Vocabulaire/src/main/java/liste/ClothingListe.java
@@ -0,0 +1,100 @@
+package liste;
+
+public class ClothingListe extends Liste {
+
+    public ClothingListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("apparel");
+        addListToEn("cap");
+        addListToEn("casual");
+        addListToEn("cleaners");
+        addListToEn("cloth");
+        addListToEn("clothes");
+        addListToEn("fabric");
+        addListToEn("fashionable");
+        addListToEn("fit");
+        addListToEn("fold");
+        addListToEn("formal");
+        addListToEn("garment");
+        addListToEn("gloves");
+        addListToEn("hanger");
+        addListToEn("hem");
+        addListToEn("lengthen");
+        addListToEn("long-sleeved");
+        addListToEn("loose-fitting");
+        addListToEn("loosen");
+        addListToEn("pants");
+        addListToEn("pattern");
+        addListToEn("piece of clothing");
+        addListToEn("pressed");
+        addListToEn("seam");
+        addListToEn("shade");
+        addListToEn("shorten");
+        addListToEn("short-sleeved");
+        addListToEn("silk");
+        addListToEn("sleeve");
+        addListToEn("smart");
+        addListToEn("stain");
+        addListToEn("stitch");
+        addListToEn("stripe");
+        addListToEn("suit");
+        addListToEn("sweater");
+        addListToEn("tan");
+        addListToEn("tie");
+        addListToEn("tight");
+        addListToEn("try on");
+        addListToEn("underwear");
+        addListToEn("wear");
+        addListToEn("wool");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("habillement");
+        addListToFr("casquette");
+        addListToFr("decontracte");
+        addListToFr("teinturier","pressing");
+        addListToFr("etoffe");
+        addListToFr("vetements");
+        addListToFr("tissu");
+        addListToFr("a la mode");
+        addListToFr("etre de la bonne taille","bien aller");
+        addListToFr("plier");
+        addListToFr("tres habille");
+        addListToFr("vetement");
+        addListToFr("gants");
+        addListToFr("cintre");
+        addListToFr("ourlet");
+        addListToFr("allonger");
+        addListToFr("a manches longues");
+        addListToFr("ample");
+        addListToFr("desserrer");
+        addListToFr("pantalon");
+        addListToFr("motif");
+        addListToFr("vetement");
+        addListToFr("repasse");
+        addListToFr("couture");
+        addListToFr("ton");
+        addListToFr("raccourcir");
+        addListToFr("a manches courtes");
+        addListToFr("soie");
+        addListToFr("manche");
+        addListToFr("elegant");
+        addListToFr("tache");
+        addListToFr("point de couture");
+        addListToFr("rayure");
+        addListToFr("costume");
+        addListToFr("pull","chandail");
+        addListToFr("beige");
+        addListToFr("cravate");
+        addListToFr("serre");
+        addListToFr("essayer");
+        addListToFr("sous-vetements");
+        addListToFr("porter");
+        addListToFr("laine");
+    }
+}
