af0c478891055e05677eca66cc07977d8848bc1b
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-17 23:06:19 +0100

    #3 feat Ajout du contenu de la liste

---
+++ Vocabulaire/src/main/java/liste/HouseListe.java
@@ -0,0 +1,92 @@
+package liste;
+
+public class HouseListe extends Liste {
+
+    public HouseListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("air-conditioner");
+        addListToEn("appliance");
+        addListToEn("attic");
+        addListToEn("bookcase");
+        addListToEn("cabinet");
+        addListToEn("carpet");
+        addListToEn("carpeting");
+        addListToEn("ceiling");
+        addListToEn("cellar");
+        addListToEn("chore");
+        addListToEn("closet");
+        addListToEn("cupboard");
+        addListToEn("curtains");
+        addListToEn("door knob","handle");
+        addListToEn("downstairs");
+        addListToEn("filing cabinet");
+        addListToEn("fitted with");
+        addListToEn("fix");
+        addListToEn("hang");
+        addListToEn("heating");
+        addListToEn("household");
+        addListToEn("landlord");
+        addListToEn("lawn");
+        addListToEn("lawnmower");
+        addListToEn("light bulb");
+        addListToEn("lighting");
+        addListToEn("painting");
+        addListToEn("plumber");
+        addListToEn("roof");
+        addListToEn("shelves");
+        addListToEn("story");
+        addListToEn("tap");
+        addListToEn("tenant");
+        addListToEn("tidy");
+        addListToEn("upstairs");
+        addListToEn("utility bill");
+        addListToEn("vacuum cleaner");
+        addListToEn("wall papering");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("climatiseur");
+        addListToFr("appareil");
+        addListToFr("grenier");
+        addListToFr("bibliotheque");
+        addListToFr("meuble de rangement");
+        addListToFr("tapis");
+        addListToFr("moquette");
+        addListToFr("plafond");
+        addListToFr("cave");
+        addListToFr("corvee");
+        addListToFr("placard");
+        addListToFr("placard");
+        addListToFr("rideaux");
+        addListToFr("poignee de porte");
+        addListToFr("en bas");
+        addListToFr("classeur");
+        addListToFr("equipe de");
+        addListToFr("reparer");
+        addListToFr("suspendre");
+        addListToFr("chauffage");
+        addListToFr("foyer");
+        addListToFr("proprietaire");
+        addListToFr("pelouse");
+        addListToFr("tondeuse a gazon");
+        addListToFr("ampoule");
+        addListToFr("eclairage");
+        addListToFr("peinture");
+        addListToFr("plombier");
+        addListToFr("toit");
+        addListToFr("etageres");
+        addListToFr("etage");
+        addListToFr("robinet");
+        addListToFr("locataire");
+        addListToFr("range","ranger");
+        addListToFr("en haut");
+        addListToFr("charges");
+        addListToFr("aspirateur");
+        addListToFr("papier peint");
+    }
+}
