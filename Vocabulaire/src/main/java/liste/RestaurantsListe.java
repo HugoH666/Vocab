37f2094f8ce14e2395ed4162212d473c2c1c5c88
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-18 22:59:43 +0100

    #7 feat Ajout du contenu de la liste

---
+++ Vocabulaire/src/main/java/liste/RestaurantsListe.java
@@ -0,0 +1,116 @@
+package liste;
+
+public class RestaurantsListe extends Liste {
+
+    public RestaurantsListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("appetizers");
+        addListToEn("bake");
+        addListToEn("beverage");
+        addListToEn("check");
+        addListToEn("chef");
+        addListToEn("china");
+        addListToEn("chopped");
+        addListToEn("clear the table");
+        addListToEn("cook");
+        addListToEn("cutlery");
+        addListToEn("dessert");
+        addListToEn("diced");
+        addListToEn("dish");
+        addListToEn("flavour");
+        addListToEn("fork");
+        addListToEn("french fries","chips");
+        addListToEn("guest");
+        addListToEn("helping");
+        addListToEn("knife");
+        addListToEn("main course");
+        addListToEn("maitre d'");
+        addListToEn("meal");
+        addListToEn("medium");
+        addListToEn("menu");
+        addListToEn("napkin");
+        addListToEn("order");
+        addListToEn("oven");
+        addListToEn("pan");
+        addListToEn("plate");
+        addListToEn("pour");
+        addListToEn("rare");
+        addListToEn("recipe");
+        addListToEn("set the table");
+        addListToEn("side order");
+        addListToEn("silver");
+        addListToEn("slice");
+        addListToEn("specialty");
+        addListToEn("spicy");
+        addListToEn("starter");
+        addListToEn("take-away");
+        addListToEn("taste");
+        addListToEn("tasteless");
+        addListToEn("tip");
+        addListToEn("today's special");
+        addListToEn("topping");
+        addListToEn("tray");
+        addListToEn("utensil");
+        addListToEn("waiter");
+        addListToEn("well done");
+        addListToEn("wine list");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("amuse-bouches","entrees");
+        addListToFr("cuire au four");
+        addListToFr("boisson");
+        addListToFr("addition");
+        addListToFr("chef");
+        addListToFr("porcelaine");
+        addListToFr("hache");
+        addListToFr("debarasser la table");
+        addListToFr("cuisinier","cuisiner");
+        addListToFr("couverts");
+        addListToFr("dessert");
+        addListToFr("coupes en des");
+        addListToFr("plat");
+        addListToFr("parfum","arome");
+        addListToFr("fourchette");
+        addListToFr("frites");
+        addListToFr("hote","client");
+        addListToFr("portion");
+        addListToFr("couteau");
+        addListToFr("plat principal");
+        addListToFr("maitre d'hotel");
+        addListToFr("repas");
+        addListToFr("a point");
+        addListToFr("carte");
+        addListToFr("serviette de table");
+        addListToFr("commande","commander");
+        addListToFr("four");
+        addListToFr("casserole");
+        addListToFr("assiette");
+        addListToFr("verser");
+        addListToFr("saignant");
+        addListToFr("recette");
+        addListToFr("mettre la table");
+        addListToFr("garniture","accompagnement");
+        addListToFr("argenterie");
+        addListToFr("tranche","trancher");
+        addListToFr("specialite");
+        addListToFr("epice");
+        addListToFr("entree");
+        addListToFr("a emporter");
+        addListToFr("gout","gouter");
+        addListToFr("insipide");
+        addListToFr("pourboire");
+        addListToFr("plat du jour");
+        addListToFr("assaisonnement");
+        addListToFr("plateau");
+        addListToFr("ustensile");
+        addListToFr("serveur");
+        addListToFr("bien cuit");
+        addListToFr("carte des vins");
+    }
+}
