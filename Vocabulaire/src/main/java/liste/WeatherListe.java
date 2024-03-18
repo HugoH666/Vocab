9d9864d4e985da3f55b2866f7a83a54143717ab3
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-18 23:11:56 +0100

    #8 feat Ajout du contenu de la liste

---
+++ Vocabulaire/src/main/java/liste/WeatherListe.java
@@ -0,0 +1,117 @@
+package liste;
+
+public class WeatherListe extends Liste {
+
+    public WeatherListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("32°F");
+        addListToEn("50°F");
+        addListToEn("60°F");
+        addListToEn("70°F");
+        addListToEn("80°F");
+        addListToEn("black ice");
+        addListToEn("blow");
+        addListToEn("breeze");
+        addListToEn("bright");
+        addListToEn("chill");
+        addListToEn("cloudy");
+        addListToEn("damp");
+        addListToEn("drought");
+        addListToEn("dull");
+        addListToEn("earthquake");
+        addListToEn("flood");
+        addListToEn("fog");
+        addListToEn("freeze");
+        addListToEn("frost");
+        addListToEn("gale");
+        addListToEn("gust");
+        addListToEn("hail");
+        addListToEn("hazard");
+        addListToEn("haze");
+        addListToEn("heat");
+        addListToEn("heat wave");
+        addListToEn("icy");
+        addListToEn("lightning");
+        addListToEn("lower seventies");
+        addListToEn("lull");
+        addListToEn("melt");
+        addListToEn("mid sixties");
+        addListToEn("mild");
+        addListToEn("mist");
+        addListToEn("outdoors");
+        addListToEn("overcast");
+        addListToEn("pour");
+        addListToEn("rainfall");
+        addListToEn("ray");
+        addListToEn("shine");
+        addListToEn("shower");
+        addListToEn("sleet");
+        addListToEn("snowbound");
+        addListToEn("spell");
+        addListToEn("storm");
+        addListToEn("swelling");
+        addListToEn("thaw");
+        addListToEn("thunderstorm");
+        addListToEn("upper eighties");
+        addListToEn("wet");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("0°C");
+        addListToFr("10°C");
+        addListToFr("15.5°C","15,5°C");
+        addListToFr("21°C");
+        addListToFr("26.6°C","26,6°C");
+        addListToFr("verglas");
+        addListToFr("souffler");
+        addListToFr("brise");
+        addListToFr("ensoleille");
+        addListToFr("refroidissement");
+        addListToFr("nuageux");
+        addListToFr("nuageux");
+        addListToFr("humide");
+        addListToFr("secheresse");
+        addListToFr("maussade");
+        addListToFr("tremblement de terre");
+        addListToFr("inondation");
+        addListToFr("brouillard");
+        addListToFr("geler");
+        addListToFr("gelee");
+        addListToFr("coup de vent");
+        addListToFr("rafale");
+        addListToFr("grele");
+        addListToFr("danger");
+        addListToFr("brume legere");
+        addListToFr("chaleur");
+        addListToFr("vague de chaleur","canicule");
+        addListToFr("verglace");
+        addListToFr("foudre");
+        addListToFr("environ 21°C");
+        addListToFr("accalmie");
+        addListToFr("fondre");
+        addListToFr("environ 15°C");
+        addListToFr("doux");
+        addListToFr("brume");
+        addListToFr("dehors");
+        addListToFr("couvert");
+        addListToFr("couler","ruisseler");
+        addListToFr("precipitation");
+        addListToFr("rayon");
+        addListToFr("briller");
+        addListToFr("averse");
+        addListToFr("gresil");
+        addListToFr("bloque par la neige");
+        addListToFr("periode");
+        addListToFr("tempete");
+        addListToFr("crue");
+        addListToFr("degeler");
+        addListToFr("orage");
+        addListToFr("environ 32°C");
+        addListToFr("mouille");
+    }
+}
