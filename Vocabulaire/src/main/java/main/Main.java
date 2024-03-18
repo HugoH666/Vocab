6af20a01add34fa1e5b5f0cce8f41b14d074d5ae
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-18 22:28:19 +0100

    #5 feat Ajout de la possibilité de choisir la liste miscellaneous

--- Vocabulaire/src/main/java/main/Main.java
+++ Vocabulaire/src/main/java/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Liste des différents thèmes
-        final String[] themes = new String[]{"Driving","Shopping","Money","House","Clothing"};
+        final String[] themes = new String[]{"Driving","Shopping","Money","House","Clothing","Miscellaneous"};
 
         Liste ll = null; // On initialise une liste null
         Random random = new Random();
@@ -33,6 +33,7 @@ public class Main {
                 case "Money": ll = new MoneyListe(); break;
                 case "House": ll = new HouseListe(); break;
                 case "Clothing": ll = new ClothingListe(); break;
+                case "Miscellaneous": ll = new MiscellaneousListe(); break;
                 default: System.out.println("\nPlease choose a theme in the following list.");
             }
         }
