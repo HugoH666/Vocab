b4607b009f7c430d908283bb8ef03e12a3031724
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-19 15:43:13 +0100

    #11 feat Ajout du contenu de la liste Phone Calls

---
+++ Vocabulaire/src/main/java/liste/PhoneCallsListe.java
@@ -0,0 +1,76 @@
+package liste;
+
+public class PhoneCallsListe extends Liste {
+
+    public PhoneCallsListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("area code");
+        addListToEn("be cut off");
+        addListToEn("beep");
+        addListToEn("busy","engaged");
+        addListToEn("call back");
+        addListToEn("cell phone");
+        addListToEn("conference call");
+        addListToEn("connection");
+        addListToEn("cut off");
+        addListToEn("dial");
+        addListToEn("emergency call");
+        addListToEn("extension");
+        addListToEn("hang up");
+        addListToEn("hold on","hold the line","stay on the line");
+        addListToEn("key");
+        addListToEn("landline");
+        addListToEn("phone directory");
+        addListToEn("press a key");
+        addListToEn("put someone through to");
+        addListToEn("recording");
+        addListToEn("ringtone");
+        addListToEn("speak up");
+        addListToEn("speaking");
+        addListToEn("spell");
+        addListToEn("subscriber");
+        addListToEn("telephone booth");
+        addListToEn("time zone");
+        addListToEn("toll-free number");
+        addListToEn("tone");
+        addListToEn("voicemail");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("indicatif");
+        addListToFr("etre coupe");
+        addListToFr("bip");
+        addListToFr("occupe");
+        addListToFr("rappeler");
+        addListToFr("telephone portable");
+        addListToFr("reunion telephonique","visio");
+        addListToFr("communication");
+        addListToFr("couper");
+        addListToFr("composer");
+        addListToFr("appel d'urgence");
+        addListToFr("poste");
+        addListToFr("raccrocher");
+        addListToFr("rester en ligne");
+        addListToFr("touche");
+        addListToFr("telephone fixe");
+        addListToFr("annuaire telephonique");
+        addListToFr("appuyer sur une touche");
+        addListToFr("passer quelqu'un a");
+        addListToFr("enregistrement");
+        addListToFr("tonalite");
+        addListToFr("parler plus fort");
+        addListToFr("a l'appareil");
+        addListToFr("epeler");
+        addListToFr("abonne");
+        addListToFr("cabine telephonique");
+        addListToFr("fuseau horaire");
+        addListToFr("numero vert");
+        addListToFr("tonalite");
+        addListToFr("messagerie vocale");
+    }
+}
