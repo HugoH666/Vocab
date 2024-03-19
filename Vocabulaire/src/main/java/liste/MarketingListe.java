571dad43c26c40da31ce0649f3c41e1f011f7071
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-19 19:03:39 +0100

    #14 feat Ajout du contenu de la liste

---
+++ Vocabulaire/src/main/java/liste/MarketingListe.java
@@ -0,0 +1,122 @@
+package liste;
+
+public class MarketingListe extends Liste {
+
+    public MarketingListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("ad","advert","advertisement");
+        addListToEn("adman");
+        addListToEn("aim");
+        addListToEn("audience");
+        addListToEn("bargain");
+        addListToEn("behaviour");
+        addListToEn("bid");
+        addListToEn("blueprint");
+        addListToEn("boost");
+        addListToEn("bracket");
+        addListToEn("brand");
+        addListToEn("broadcast");
+        addListToEn("brochure");
+        addListToEn("commercial");
+        addListToEn("consumer");
+        addListToEn("consumer goods");
+        addListToEn("contest");
+        addListToEn("data");
+        addListToEn("data processing");
+        addListToEn("database");
+        addListToEn("disposable");
+        addListToEn("draft");
+        addListToEn("flyer","leaflet");
+        addListToEn("hoarding","billboard");
+        addListToEn("household");
+        addListToEn("launch");
+        addListToEn("layout");
+        addListToEn("literature");
+        addListToEn("loyalty");
+        addListToEn("market share");
+        addListToEn("misleading");
+        addListToEn("niche");
+        addListToEn("poll","survey");
+        addListToEn("poster");
+        addListToEn("press release");
+        addListToEn("purchasing power");
+        addListToEn("rate");
+        addListToEn("rebate");
+        addListToEn("run an ad");
+        addListToEn("sample");
+        addListToEn("sampling");
+        addListToEn("sketch");
+        addListToEn("sponsor");
+        addListToEn("sponsorship");
+        addListToEn("sticker");
+        addListToEn("tap a market");
+        addListToEn("target");
+        addListToEn("taste");
+        addListToEn("trade advertising");
+        addListToEn("trade mark");
+        addListToEn("trend");
+        addListToEn("upscale");
+        addListToEn("user");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("annonce publicitaire");
+        addListToFr("publicitaire");
+        addListToFr("but","viser");
+        addListToFr("public","audience");
+        addListToFr("bonne affaire","negocier");
+        addListToFr("comportement");
+        addListToFr("offre");
+        addListToFr("plan","avant-projet");
+        addListToFr("promouvoir");
+        addListToFr("tranche");
+        addListToFr("marque");
+        addListToFr("diffuser");
+        addListToFr("brochure");
+        addListToFr("spot publicitaire");
+        addListToFr("consommateur");
+        addListToFr("biens de consommation");
+        addListToFr("concours");
+        addListToFr("donnees");
+        addListToFr("traitement des donnees");
+        addListToFr("base de donnees");
+        addListToFr("jetable");
+        addListToFr("brouillon");
+        addListToFr("prospectus");
+        addListToFr("panneau publicitaire");
+        addListToFr("menage");
+        addListToFr("lancement","lancer");
+        addListToFr("presentation","disposition");
+        addListToFr("documentation");
+        addListToFr("fidelite");
+        addListToFr("part de marche");
+        addListToFr("trompeur");
+        addListToFr("creneau");
+        addListToFr("sondage","sonder");
+        addListToFr("affiche");
+        addListToFr("communique de presse");
+        addListToFr("pouvoir d'achat");
+        addListToFr("tarif","taux");
+        addListToFr("rabais");
+        addListToFr("passer une annonce");
+        addListToFr("echantillon");
+        addListToFr("echantillonnage");
+        addListToFr("croquis");
+        addListToFr("sponsor","sponsoriser");
+        addListToFr("sponsoring");
+        addListToFr("autocollant");
+        addListToFr("exploiter un marche");
+        addListToFr("cible","cibler");
+        addListToFr("gout","gouter");
+        addListToFr("publicite professionnelle");
+        addListToFr("marque");
+        addListToFr("tendance");
+        addListToFr("haut de gamme");
+        addListToFr("utilisateur");
+    }
+}
