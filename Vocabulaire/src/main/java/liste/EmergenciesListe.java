778cd138a80d2f2f8a75e95d715f5f24f6a453c7
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-19 16:19:24 +0100

    #16 feat Ajout du contenu de la liste

---
+++ Vocabulaire/src/main/java/liste/EmergenciesListe.java
@@ -0,0 +1,93 @@
+package liste;
+
+public class EmergenciesListe extends Liste {
+
+    public EmergenciesListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("airlift");
+        addListToEn("avoid");
+        addListToEn("blast");
+        addListToEn("burn");
+        addListToEn("burst");
+        addListToEn("casualty");
+        addListToEn("collapse");
+        addListToEn("crack");
+        addListToEn("crumble");
+        addListToEn("damage");
+        addListToEn("damages");
+        addListToEn("delay");
+        addListToEn("drift");
+        addListToEn("dwindle");
+        addListToEn("failure");
+        addListToEn("fire hydrant");
+        addListToEn("flood");
+        addListToEn("give way");
+        addListToEn("gush");
+        addListToEn("harmful","hazardous");
+        addListToEn("hazard");
+        addListToEn("hurricane");
+        addListToEn("injury");
+        addListToEn("leak");
+        addListToEn("mud");
+        addListToEn("out of service");
+        addListToEn("power");
+        addListToEn("power blackout");
+        addListToEn("power surge");
+        addListToEn("relief");
+        addListToEn("rescuer");
+        addListToEn("resume");
+        addListToEn("rip");
+        addListToEn("smash into");
+        addListToEn("storm");
+        addListToEn("stretcher");
+        addListToEn("subside");
+        addListToEn("trigger");
+        addListToEn("twister");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("evacuer par pont aerien");
+        addListToFr("eviter");
+        addListToFr("detonation");
+        addListToFr("bruler");
+        addListToFr("eclater");
+        addListToFr("victime");
+        addListToFr("effoncrement","s'effondrer");
+        addListToFr("fissure");
+        addListToFr("s'effriter");
+        addListToFr("degats");
+        addListToFr("dommages et interets");
+        addListToFr("retard");
+        addListToFr("derive","deriver");
+        addListToFr("s'affaiblir");
+        addListToFr("defaillance");
+        addListToFr("bouche d'incendie");
+        addListToFr("inondation");
+        addListToFr("ceder");
+        addListToFr("jaillir");
+        addListToFr("dangereux");
+        addListToFr("danger");
+        addListToFr("ouragan");
+        addListToFr("blessure");
+        addListToFr("fuite","fuir");
+        addListToFr("boue");
+        addListToFr("hors service");
+        addListToFr("courant");
+        addListToFr("panne de courant");
+        addListToFr("surtension");
+        addListToFr("sauveteur");
+        addListToFr("reprendre","recommencer");
+        addListToFr("dechirer");
+        addListToFr("s'ecraser contre");
+        addListToFr("tempete");
+        addListToFr("civiere");
+        addListToFr("se calmer");
+        addListToFr("declencher");
+        addListToFr("tornade");
+    }
+}
