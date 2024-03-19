ef5699d4616f2b0965b456d1b79161fcd8bc64ba
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-19 19:22:16 +0100

    #15 feat Ajout du contenu de la liste

---
+++ Vocabulaire/src/main/java/liste/FactoryListe.java
@@ -0,0 +1,148 @@
+package liste;
+
+public class FactoryListe extends Liste {
+
+    public FactoryListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("apparatus");
+        addListToEn("assembly line");
+        addListToEn("battery");
+        addListToEn("beam");
+        addListToEn("belt");
+        addListToEn("bolt");
+        addListToEn("broom");
+        addListToEn("can");
+        addListToEn("check");
+        addListToEn("chemist");
+        addListToEn("circuit");
+        addListToEn("conveyor belt");
+        addListToEn("damage");
+        addListToEn("device");
+        addListToEn("drill");
+        addListToEn("facilities");
+        addListToEn("factory","plant");
+        addListToEn("failure");
+        addListToEn("faulty");
+        addListToEn("fix");
+        addListToEn("fluid once","floz");
+        addListToEn("fold");
+        addListToEn("fuel");
+        addListToEn("fuse");
+        addListToEn("gallon");
+        addListToEn("halt");
+        addListToEn("handle");
+        addListToEn("handling");
+        addListToEn("hose");
+        addListToEn("inch");
+        addListToEn("lab work");
+        addListToEn("ladder");
+        addListToEn("leak");
+        addListToEn("lever");
+        addListToEn("lift");
+        addListToEn("load");
+        addListToEn("log");
+        addListToEn("mechanic");
+        addListToEn("meter");
+        addListToEn("monitor");
+        addListToEn("nut");
+        addListToEn("ounce");
+        addListToEn("out of service");
+        addListToEn("overhaul");
+        addListToEn("panel");
+        addListToEn("pipe");
+        addListToEn("pound");
+        addListToEn("power");
+        addListToEn("premises");
+        addListToEn("pull");
+        addListToEn("repair man");
+        addListToEn("replacement part","spare part");
+        addListToEn("sample");
+        addListToEn("screw");
+        addListToEn("screwdriver");
+        addListToEn("scrub");
+        addListToEn("shelf");
+        addListToEn("shift");
+        addListToEn("spread");
+        addListToEn("store");
+        addListToEn("switch");
+        addListToEn("switchboard");
+        addListToEn("trigger");
+        addListToEn("warehouse");
+        addListToEn("waste disposal");
+        addListToEn("yard");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("appareil");
+        addListToFr("chaine de montage");
+        addListToFr("pile");
+        addListToFr("poutre","rayon");
+        addListToFr("courroie");
+        addListToFr("boulon");
+        addListToFr("balai");
+        addListToFr("boite en fer","mettre en boite");
+        addListToFr("verifier");
+        addListToFr("chimiste");
+        addListToFr("circuit");
+        addListToFr("tapis roulant");
+        addListToFr("degats");
+        addListToFr("systeme","dispositif");
+        addListToFr("forer","percer");
+        addListToFr("installations");
+        addListToFr("usine");
+        addListToFr("echec","panne");
+        addListToFr("defectueux");
+        addListToFr("reparer");
+        addListToFr("once liquide","28.4ml","28,4ml");
+        addListToFr("plier");
+        addListToFr("carburant");
+        addListToFr("fusible");
+        addListToFr("gallon","3.785L","3,785L");
+        addListToFr("arreter");
+        addListToFr("poignee","manipuler");
+        addListToFr("manutention");
+        addListToFr("tuyau");
+        addListToFr("pouce","2.54cm","2,54cm");
+        addListToFr("travail de laboratoire");
+        addListToFr("echelle");
+        addListToFr("fuite","fuir");
+        addListToFr("levier");
+        addListToFr("soulever");
+        addListToFr("charge","charger");
+        addListToFr("noter","enregistrer");
+        addListToFr("mecanicien");
+        addListToFr("compteur");
+        addListToFr("moteur","controler");
+        addListToFr("ecrou");
+        addListToFr("once","28.35g","28,35g");
+        addListToFr("en panne");
+        addListToFr("reviser");
+        addListToFr("panneau");
+        addListToFr("canalisation");
+        addListToFr("livre","453.6g","453,6g");
+        addListToFr("courant electrique");
+        addListToFr("lieux");
+        addListToFr("tirer");
+        addListToFr("reparateur");
+        addListToFr("piece de rechange");
+        addListToFr("echantillon");
+        addListToFr("vis");
+        addListToFr("tournevis");
+        addListToFr("nettoyer");
+        addListToFr("etagere");
+        addListToFr("equipe de travail");
+        addListToFr("etaler");
+        addListToFr("stocker");
+        addListToFr("interrupteur","permuter");
+        addListToFr("panneau de commandes");
+        addListToFr("declencher");
+        addListToFr("entrepot");
+        addListToFr("traitement des dechets");
+        addListToFr("yard","91.44cm","91,44cm");
+    }
+}
