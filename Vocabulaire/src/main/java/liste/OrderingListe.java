72dfa9d01f36e0fa0539ee4c2b6498a9762181f8
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-19 16:06:48 +0100

    #12 feat Ajout du contenu de la liste Ordering

---
+++ Vocabulaire/src/main/java/liste/OrderingListe.java
@@ -0,0 +1,124 @@
+package liste;
+
+public class OrderingListe extends Liste {
+
+    public OrderingListe() {
+        super();
+    }
+
+    @Override
+    public void fillEnListe() {
+        addListToEn("amount");
+        addListToEn("arrange for");
+        addListToEn("backlog");
+        addListToEn("behind schedule","overdue");
+        addListToEn("bid");
+        addListToEn("bill","invoice");
+        addListToEn("billing");
+        addListToEn("charge");
+        addListToEn("check");
+        addListToEn("commodity");
+        addListToEn("complaint");
+        addListToEn("consignment");
+        addListToEn("courier");
+        addListToEn("credit note");
+        addListToEn("damaged");
+        addListToEn("deadline");
+        addListToEn("deduct");
+        addListToEn("delay");
+        addListToEn("deliver");
+        addListToEn("discrepancy");
+        addListToEn("dispatch");
+        addListToEn("draft");
+        addListToEn("forward");
+        addListToEn("handling");
+        addListToEn("in bulk");
+        addListToEn("inconvenience");
+        addListToEn("inquiry");
+        addListToEn("item");
+        addListToEn("lead time");
+        addListToEn("lorry");
+        addListToEn("mailman");
+        addListToEn("order");
+        addListToEn("out of stock");
+        addListToEn("overnight mail");
+        addListToEn("package");
+        addListToEn("parcel");
+        addListToEn("pick up");
+        addListToEn("place an order");
+        addListToEn("postpone");
+        addListToEn("process");
+        addListToEn("quotation");
+        addListToEn("regarding");
+        addListToEn("registered mail");
+        addListToEn("sample");
+        addListToEn("shipment");
+        addListToEn("shortage");
+        addListToEn("short of");
+        addListToEn("sort");
+        addListToEn("stamp");
+        addListToEn("trial order");
+        addListToEn("unload");
+        addListToEn("unpack");
+        addListToEn("warehouse");
+        addListToEn("wrap");
+    }
+
+    @Override
+    public void fillFrListe() {
+        addListToFr("montant");
+        addListToFr("organiser","fixer");
+        addListToFr("arriere");
+        addListToFr("en retard");
+        addListToFr("offre","faire une offre");
+        addListToFr("facture");
+        addListToFr("facturation");
+        addListToFr("facturer");
+        addListToFr("verifier");
+        addListToFr("denree");
+        addListToFr("reclamation");
+        addListToFr("envoi");
+        addListToFr("coursier");
+        addListToFr("avoir");
+        addListToFr("endommage");
+        addListToFr("date limite");
+        addListToFr("deduire");
+        addListToFr("retard");
+        addListToFr("livrer");
+        addListToFr("difference");
+        addListToFr("expedier");
+        addListToFr("traite");
+        addListToFr("envoyer");
+        addListToFr("manutention");
+        addListToFr("en nombre");
+        addListToFr("inconvenient","gene");
+        addListToFr("demande de renseignements");
+        addListToFr("article");
+        addListToFr("delai");
+        addListToFr("camion");
+        addListToFr("facteur");
+        addListToFr("commande","commander");
+        addListToFr("en rupture de stock");
+        addListToFr("courrier qui arrive le lendemain");
+        addListToFr("paquet");
+        addListToFr("colis");
+        addListToFr("ramasser","aller chercher");
+        addListToFr("passer une commande");
+        addListToFr("retarder","reporter");
+        addListToFr("processus","traitement","traiter");
+        addListToFr("devis","prix");
+        addListToFr("concernant");
+        addListToFr("courrier recommande");
+        addListToFr("echantillon");
+        addListToFr("expedition");
+        addListToFr("penurie");
+        addListToFr("a court de");
+        addListToFr("trier");
+        addListToFr("timbre","affranchir");
+        addListToFr("commande d'essai");
+        addListToFr("decharger");
+        addListToFr("deballer");
+        addListToFr("entrepot");
+        addListToFr("emballer");
+    }
+}
